17c3d18cece06ed8322efeb8f15cd7e909db677a
nikolai <kol9dana6262ak1@gmail.com>
2023-02-23 18:31:48 +0300

    add example 5

---
+++ example_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "вероника")
+{
+    Console.WriteLine ("Привет Ничка!!!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
